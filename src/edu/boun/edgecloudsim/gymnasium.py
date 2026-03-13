00ad67b8ada8c5c64a3043b67958439d5df5a7f6
Arun Murugan <arun.han.murugan@gmail.com>
2026-03-13 12:55:40 -0700

    moved file into dag directory, added function for action masking, added get info function for debugging

--- src/edu/boun/edgecloudsim/gymnasium.py
+++ src/edu/boun/edgecloudsim/gymnasium.py
@@ -1,16 +1,18 @@
 import numpy as np
-import gymnasium as gym
-from gymnasium import spaces
+import edu.boun.edgecloudsim.gymnasium as gym
+from edu.boun.edgecloudsim.gymnasium import spaces
 
 class SchedulingEnvironment(gym.Env):
     def __init__(self, num_dc = 8, num_dags=1000):
-        super.__init__()
+        super().__init__()
         self.num_dc = num_dc
         self.num_dags = num_dags
         self.alpha = 0.1
         self.current_step = 0
         self.max_steps = 1000
         self.completed_dags = 0
+        self.current_cost = 0
+        self.current_time = 0
 
         OBSERVATION_SIZE = 2 + (5 * num_dc)
 
@@ -26,10 +28,15 @@ class SchedulingEnvironment(gym.Env):
         #return observation and information
         super().reset(seed=seed)
         self.current_step = 0
+        self.completed_dags = 0
+        self.current_cost = 0
+        self.current_time = 0
         obs = self._get_obs()
-        return obs, {}
+        info = self._get_info()
+        
+        return obs, info
 
-    def step(self):
+    def step(self, action):
         #action
         #send to edgecloudsim
         #get cost, time
@@ -48,7 +55,7 @@ class SchedulingEnvironment(gym.Env):
         terminated = self._is_terminated()
         obs = self._get_obs()
 
-        info = {}
+        info = self._get_info()
 
         return obs, reward, terminated, False, info
 
@@ -56,10 +63,13 @@ class SchedulingEnvironment(gym.Env):
         #internal state to observation state representation, do not concern about vms within data center
 
         #features of current task
+        task_features = []
         #load on cpu
         task_cpu = np.random.uniform(0, 1)
         #load on memory
         task_memory = np.random.uniform(0, 1)
+        task_features.append(task_cpu)
+        task_features.append(task_memory)
 
         dc_features = []
         #features of data center
@@ -71,10 +81,33 @@ class SchedulingEnvironment(gym.Env):
             queue_length = np.random.uniform(0, 1) #number of tasks waiting to start
             dc_features.extend([available_nodes, utilization_capacity, available_ram, available_cpu, queue_length])
 
-        obs = np.array(task_cpu + task_memory + dc_features, dtype=np.float32)
+        obs = np.array(task_features + dc_features, dtype=np.float32)
         
         return obs
 
     def _is_terminated(self):
         return self.completed_dags == self.num_dags
 
+    #action masking
+    def _get_action_mask(self):
+        mask = []
+        #check if current data center is available to schedule
+        for i in range(self.num_dc):
+            #is_available: True or False
+            is_available = True
+            mask.append(is_available)
+        
+        mask = np.array(mask, dtype=bool)
+
+        if not any(mask):
+            mask[0] = True
+        
+        return mask
+    
+    def _get_info(self):
+        return {
+            "action_mask": self._get_action_mask(),
+            "dags_completed": self.completed_dags,
+            "cost": self.current_cost,
+            "time_taken": self.current_time
+        }
